edf63de894b81675fb1a321ae0980e9e71cbca09
Gilberto Ocampo <ocampo@gate.net>
2020-03-28 22:26:53 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ end
 
 def age_appropriate(birthday_kids)
   birthday_kids.each do |kids_name, age|
-      if age < 12 puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+      if birthday_kids[age] < 12 puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end 
 
 
